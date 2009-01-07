e3bebfeafcbc764fda0de365f23f0ee30f39d72c
Olli Pettay <Olli.Pettay@helsinki.fi>
2009-01-07 12:21:24 +0200

    Bug 436965, r+sr=sicking, a=blocking

--- layout/base/nsPresShell.cpp
+++ layout/base/nsPresShell.cpp
@@ -2410,7 +2410,10 @@ PresShell::InitialReflow(nscoord aWidth, nscoord aHeight)
 
     // Now flush out pending restyles before we actually reflow, in
     // case XBL constructors changed styles somewhere.
-    mFrameConstructor->ProcessPendingRestyles();
+    {
+      nsAutoScriptBlocker scriptBlocker;
+      mFrameConstructor->ProcessPendingRestyles();
+    }
 
     // And that might have run _more_ XBL constructors
     NS_ENSURE_STATE(!mHaveShutDown);
@@ -2513,7 +2516,10 @@ PresShell::ResizeReflow(nscoord aWidth, nscoord aHeight)
     nsCOMPtr<nsIPresShell> kungFuDeathGrip(this);
 
     // Make sure style is up to date
-    mFrameConstructor->ProcessPendingRestyles();
+    {
+      nsAutoScriptBlocker scriptBlocker;
+      mFrameConstructor->ProcessPendingRestyles();
+    }
 
     if (!mIsDestroying) {
       // XXX Do a full invalidate at the beginning so that invalidates along
@@ -4510,6 +4516,7 @@ PresShell::DoFlushPendingNotifications(mozFlushType aType,
       // reflow).
       mPresContext->FlushUserFontSet();
 
+      nsAutoScriptBlocker scriptBlocker;
       mFrameConstructor->ProcessPendingRestyles();
     }
 
@@ -4527,6 +4534,7 @@ PresShell::DoFlushPendingNotifications(mozFlushType aType,
     // up the new rules and we'll end up with frames whose style doesn't match
     // the frame type.
     if (!mIsDestroying) {
+      nsAutoScriptBlocker scriptBlocker;
       mFrameConstructor->ProcessPendingRestyles();
     }
 
