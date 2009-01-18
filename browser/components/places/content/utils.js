6c77377dfbf3c9984b946c38b08465bc4e18bec9
Dão Gottwald <dao@mozilla.com>
2009-01-18 11:36:01 +0100

    Bug 449359 - Middle-click on empty folder in Bookmarks Toolbar or Library creates empty new tab. r=mak77+gavin

--- browser/components/places/content/utils.js
+++ browser/components/places/content/utils.js
@@ -889,6 +889,9 @@ var PlacesUIUtils = {
     * {uri: string, isBookmark: boolean}
     */
   _openTabset: function PU__openTabset(aItemsToOpen, aEvent) {
+    if (!aItemsToOpen.length)
+      return;
+
     var urls = [];
     for (var i = 0; i < aItemsToOpen.length; i++) {
       var item = aItemsToOpen[i];
