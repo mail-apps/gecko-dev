c297e1fde2daa129b6d317288885c6dfceef38b8
Boris Zbarsky <bzbarsky@mit.edu>
2009-02-24 13:39:09 -0500

    Bug 476245.  Keep better track of our anonymous content.  r=smaug, sr=jst

--- content/base/public/nsIContent.h
+++ content/base/public/nsIContent.h
@@ -148,7 +148,7 @@ public:
    */
   PRBool IsRootOfNativeAnonymousSubtree() const
   {
-    return HasFlag(NODE_IS_ANONYMOUS);
+    return HasFlag(NODE_IS_ANONYMOUS) && HasFlag(NODE_IS_IN_ANONYMOUS_SUBTREE);
   }
 
   /**
@@ -199,8 +199,12 @@ public:
                  (GetParent() && GetBindingParent() == GetParent()),
                  "root of native anonymous subtree must have parent equal "
                  "to binding parent");
-    nsIContent *bindingParent = GetBindingParent();
-    return bindingParent && bindingParent == GetParent();
+    NS_ASSERTION(!GetParent() ||
+                 ((GetBindingParent() == GetParent()) ==
+                  HasFlag(NODE_IS_ANONYMOUS)),
+                 "For nodes with parent, flag and GetBindingParent() check "
+                 "should match");
+    return HasFlag(NODE_IS_ANONYMOUS);
   }
 
   /**
