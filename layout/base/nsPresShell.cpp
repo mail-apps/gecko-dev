60ed535fce25e9f9e28505792110165bf9b454d7
Zack Weinberg <zweinberg@mozilla.com>
2009-02-25 09:35:31 -0800

    Bug 478784: fix transparent-widget check in PresShell::Paint; r+sr=roc
    --HG--
    extra : rebase_source : a89293f6889722645d15588d8f45a86ccf980c7f

--- layout/base/nsPresShell.cpp
+++ layout/base/nsPresShell.cpp
@@ -5394,11 +5394,12 @@ PresShell::Paint(nsIView*             aView,
   // background color as recorded in the prescontext is guaranteed to
   // be opaque.
 
+  PRBool needTransparency = PR_FALSE;
   nscolor backgroundColor = mPresContext->DefaultBackgroundColor();
   for (nsIView *view = aView; view; view = view->GetParent()) {
     if (view->HasWidget() &&
         view->GetWidget()->GetTransparencyMode() != eTransparencyOpaque) {
-      backgroundColor = NS_RGBA(0,0,0,0);
+      needTransparency = PR_TRUE;
       break;
     }
   }
@@ -5419,9 +5420,11 @@ PresShell::Paint(nsIView*             aView,
   // backstop colors.
   nsIFrame* frame = static_cast<nsIFrame*>(aView->GetClientData());
   if (!frame) {
-    backgroundColor = NS_ComposeColors(backgroundColor, viewDefaultColor);
-    aRenderingContext->SetColor(backgroundColor);
-    aRenderingContext->FillRect(aDirtyRegion.GetBounds());
+    if (!needTransparency) {
+      backgroundColor = NS_ComposeColors(backgroundColor, viewDefaultColor);
+      aRenderingContext->SetColor(backgroundColor);
+      aRenderingContext->FillRect(aDirtyRegion.GetBounds());
+    }
     return NS_OK;
   }
 
@@ -5445,7 +5448,8 @@ PresShell::Paint(nsIView*             aView,
   }
 
   nsLayoutUtils::PaintFrame(aRenderingContext, frame, aDirtyRegion,
-                            backgroundColor);
+                            needTransparency ? NS_RGBA(0,0,0,0)
+                            : backgroundColor);
   return NS_OK;
 }
 
