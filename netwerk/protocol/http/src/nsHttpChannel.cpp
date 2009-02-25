fa131af7c131b1248f619af79806d3a75cd89e7e
Jonas Sicking <jonas@sicking.cc>
2009-02-25 00:20:25 -0800

    Backed out changeset 223c4d40bb76 (bug 479521)

--- netwerk/protocol/http/src/nsHttpChannel.cpp
+++ netwerk/protocol/http/src/nsHttpChannel.cpp
@@ -1045,9 +1045,6 @@ nsHttpChannel::ProcessNormal()
 nsresult
 nsHttpChannel::PromptTempRedirect()
 {
-    if (!gHttpHandler->PromptTempRedirect()) {
-        return NS_OK;
-    }
     nsresult rv;
     nsCOMPtr<nsIStringBundleService> bundleService =
             do_GetService(NS_STRINGBUNDLE_CONTRACTID, &rv);
