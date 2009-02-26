9beeee5c78d858a06b92aa5dd03b70b0134ab812
Olli Pettay <Olli.Pettay@helsinki.fi>
2009-02-26 18:38:35 +0200

    Bug 466856 -  Crash [@ nsTypedSelection::CopyRangeToAnchorFocus], r+sr=roc

--- layout/generic/nsSelection.cpp
+++ layout/generic/nsSelection.cpp
@@ -5699,6 +5699,7 @@ utility function
 NS_IMETHODIMP
 nsTypedSelection::CopyRangeToAnchorFocus(nsIDOMRange *aRange)
 {
+  NS_ENSURE_STATE(mAnchorFocusRange);
   nsCOMPtr<nsIDOMNode> startNode;
   nsCOMPtr<nsIDOMNode> endNode;
   PRInt32 startOffset;
