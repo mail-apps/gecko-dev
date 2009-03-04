76fa9bdd8792f65b9c79578bedb7670b26f314fb
Wes Garland <wes@page.ca>
2009-03-03 18:15:30 -0600

    Bug 481260 - JIT stats in tracemonkey can interfere with stdout-using embeddings. r=jorendorff.

--- js/src/jstracer.cpp
+++ js/src/jstracer.cpp
@@ -4531,7 +4531,7 @@ extern void
 js_FinishJIT(JSTraceMonitor *tm)
 {
 #ifdef JS_JIT_SPEW
-    if (jitstats.recorderStarted) {
+    if (js_verboseDebug && jitstats.recorderStarted) {
         printf("recorder: started(%llu), aborted(%llu), completed(%llu), different header(%llu), "
                "trees trashed(%llu), slot promoted(%llu), unstable loop variable(%llu), "
                "breaks(%llu), returns(%llu), unstableInnerCalls(%llu)\n",
