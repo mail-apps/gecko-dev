415cfc4c290614d67c32e41733bcdd17bf20b62a
Joe Drew <joe@drew.ca>
2009-03-17 17:07:16 -0400

    Bug 479328 and bug 481753 - Ensure imgRequest always knows when it's in the cache, and doesn't try to do cache manipulation when it isn't. Also, fix redirect handling to not simply invalidate the cache entry. r=vlad, sr=bzbarsky a=blocking1.9.1+

--- modules/libpr0n/src/imgRequest.h
+++ modules/libpr0n/src/imgRequest.h
@@ -168,10 +168,10 @@ private:
   // Return whether we've seen some data at this point
   PRBool HasTransferredData() const { return mGotData; }
 
-  // Set whether this request is cacheable. By default, all requests are
-  // cacheable, but they might not be if there is already a request with this
-  // key URI in the cache.
-  void SetCacheable(PRBool cacheable);
+  // Set whether this request is stored in the cache. If it isn't, regardless
+  // of whether this request has a non-null mCacheEntry, this imgRequest won't
+  // try to update or modify the image cache.
+  void SetIsInCache(PRBool cacheable);
 
 public:
   NS_DECL_IMGILOAD
@@ -218,7 +218,7 @@ private:
   PRPackedBool mProcessing : 1;
   PRPackedBool mHadLastPart : 1;
   PRPackedBool mGotData : 1;
-  PRPackedBool mIsCacheable : 1;
+  PRPackedBool mIsInCache : 1;
 };
 
 #endif
