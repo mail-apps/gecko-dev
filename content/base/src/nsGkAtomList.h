b841addbc33ff19ae8db990630dd029b64e7cb68
Mats Palmgren <mats.palmgren@bredband.net>
2009-03-24 13:55:29 +1300

    Bug 382369. Distinguish click from click-drag on a <label> so text selection works. r+sr=roc, help from Zack Weinberg

--- content/base/src/nsGkAtomList.h
+++ content/base/src/nsGkAtomList.h
@@ -1643,6 +1643,7 @@ GK_ATOM(viewProperty, "ViewProperty")
 
 // Content property names
 GK_ATOM(genConInitializerProperty, "QuoteNodeProperty")
+GK_ATOM(labelMouseDownPtProperty, "LabelMouseDownPtProperty")
 
 // Languages for lang-specific transforms
 GK_ATOM(Japanese, "ja")
