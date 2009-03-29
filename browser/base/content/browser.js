2cdbf39c842ade0f1786f1a8144b02497db6d1a2
Simon Bünzli <zeniko@gmail.com>
2009-03-29 12:46:09 +0200

    Bug 479408 - search engine discovery code doesn't properly handle frames ("browser is null" in BrowserSearch::addEngine). r=gavin

--- browser/base/content/browser.js
+++ browser/base/content/browser.js
@@ -2920,6 +2920,9 @@ const BrowserSearch = {
       return;
 
     var browser = gBrowser.getBrowserForDocument(targetDoc);
+    // ignore search engines from subframes (see bug 479408)
+    if (!browser)
+      return;
 
     // Check to see whether we've already added an engine with this title
     if (browser.engines) {
@@ -2931,8 +2934,8 @@ const BrowserSearch = {
     // Use documentURIObject in the check for shouldLoadFavIcon so that we
     // do the right thing with about:-style error pages.  Bug 453442
     var iconURL = null;
-    if (gBrowser.shouldLoadFavIcon(browser.contentDocument.documentURIObject))
-      iconURL = browser.currentURI.prePath + "/favicon.ico";
+    if (gBrowser.shouldLoadFavIcon(targetDoc.documentURIObject))
+      iconURL = targetDoc.documentURIObject.prePath + "/favicon.ico";
 
     var hidden = false;
     // If this engine (identified by title) is already in the list, add it
@@ -6167,7 +6170,7 @@ var FeedHandler = {
       // find which tab this is for, and set the attribute on the browser
       var browserForLink = gBrowser.getBrowserForDocument(targetDoc);
       if (!browserForLink) {
-        // ??? this really shouldn't happen..
+        // ignore feeds loaded in subframes (see bug 305472)
         return;
       }
 
@@ -6177,7 +6180,7 @@ var FeedHandler = {
 
       feeds.push(feed);
       browserForLink.feeds = feeds;
-      if (browserForLink == gBrowser || browserForLink == gBrowser.mCurrentBrowser) {
+      if (browserForLink == gBrowser.mCurrentBrowser) {
         var feedButton = document.getElementById("feed-button");
         if (feedButton) {
           feedButton.setAttribute("feeds", "true");
