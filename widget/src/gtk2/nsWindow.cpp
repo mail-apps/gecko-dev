8175a3b103fadea3f194d6d02f5ab804972cc18a
Ginn Chen <ginn.chen@sun.com>
2009-03-31 18:03:41 +0800

    Bug 484932 Printing Sun Java System Communications Express page caused a crash [@ moz_drawingarea_resize] r=roc

--- widget/src/gtk2/nsWindow.cpp
+++ widget/src/gtk2/nsWindow.cpp
@@ -4150,7 +4150,9 @@ nsWindow::NativeResize(PRInt32 aWidth, PRInt32 aHeight, PRBool  aRepaint)
         gtk_widget_size_allocate(GTK_WIDGET(mContainer), &allocation);
     }
 
-    moz_drawingarea_resize (mDrawingarea, aWidth, aHeight);
+    if (mDrawingarea) {
+        moz_drawingarea_resize (mDrawingarea, aWidth, aHeight);
+    }
 }
 
 void
