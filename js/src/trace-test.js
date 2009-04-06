daf9e0c02c76c1c3f314950f507512f078da48ff
Jason Orendorff <jorendorff@mozilla.com>
2009-04-06 13:24:20 -0500

    Bug 479198 - "Deep" property cache entries not invalidated when shadowed (TIBCO General Interface regression). r=brendan.

--- js/src/trace-test.js
+++ js/src/trace-test.js
@@ -4938,6 +4938,23 @@ function testLengthOnNonNativeProto()
 testLengthOnNonNativeProto.expected = "no assertion";
 test(testLengthOnNonNativeProto);
 
+function testDeepPropertyShadowing()
+{
+    function h(node) {
+        var x = 0;
+        while (node) {
+            x++;
+            node = node.parent;
+        }
+        return x;
+    }
+    var tree = {__proto__: {__proto__: {parent: null}}};
+    h(tree);
+    h(tree);
+    tree.parent = {};
+    assertEq(h(tree), 2);
+}
+test(testDeepPropertyShadowing);
 
 /*****************************************************************************
  *                                                                           *
