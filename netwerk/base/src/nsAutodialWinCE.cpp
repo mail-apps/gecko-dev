e7680a61c9552784f6b17e87e3598edc9fb90486
Doug Turner <dougt@meer.net>
2009-04-13 21:41:33 -0700

    Bug 488140 - rename the class in nsAutodial and friends to be consistent. r=jduell, sr=stuart

--- netwerk/base/src/nsAutodialWinCE.cpp
+++ netwerk/base/src/nsAutodialWinCE.cpp
@@ -47,24 +47,24 @@
 
 
 // pulled from the header so that we do not get multiple define errors during link
-static const GUID ras_DestNetInternet =
+static const GUID autodial_DestNetInternet =
         { 0x436ef144, 0xb4fb, 0x4863, { 0xa0, 0x41, 0x8f, 0x90, 0x5a, 0x62, 0xc5, 0x72 } };
 
-nsRASAutodial::nsRASAutodial()
+nsAutodial::nsAutodial()
 {
 }
 
-nsRASAutodial::~nsRASAutodial()
+nsAutodial::~nsAutodial()
 {
 }
 
 nsresult
-nsRASAutodial::Init()
+nsAutodial::Init()
 {
   return NS_OK;
 }
 
-nsresult nsRASAutodial::DialDefault(const PRUnichar* /* hostName */)
+nsresult nsAutodial::DialDefault(const PRUnichar* /* hostName */)
 {
   HANDLE connectionHandle;
 
@@ -75,7 +75,7 @@ nsresult nsRASAutodial::DialDefault(const PRUnichar* /* hostName */)
   conn_info.cbSize      = sizeof(conn_info);
   conn_info.dwParams    = CONNMGR_PARAM_GUIDDESTNET;
   conn_info.dwPriority  = CONNMGR_PRIORITY_USERINTERACTIVE;
-  conn_info.guidDestNet = ras_DestNetInternet;
+  conn_info.guidDestNet = autodial_DestNetInternet;
   conn_info.bExclusive  = FALSE;
   conn_info.bDisabled   = FALSE;
   
@@ -104,7 +104,7 @@ nsresult nsRASAutodial::DialDefault(const PRUnichar* /* hostName */)
 }
 
 PRBool
-nsRASAutodial::ShouldDialOnNetworkError()
+nsAutodial::ShouldDialOnNetworkError()
 {
   return PR_TRUE;
 }
