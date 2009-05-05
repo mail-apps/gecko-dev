b225edf725aa31e71159edc6dae8cdc5b3683cdf
Serge Gautherie <sgautherie.bz@free.fr>
2009-05-05 16:10:17 +0200

    Bug 485736 - Add (TUnit) 'xpcshell-tests' |make| target, using |runxpcshelltests.py| new '--manifest' option; (Iv1a-MC) Update '.PHONY' target too; r=ted.mielczarek

--- js/src/config/rules.mk
+++ js/src/config/rules.mk
@@ -202,6 +202,8 @@ check::
 
 endif # CPP_UNIT_TESTS
 
+.PHONY: check xpcshell-tests check-interactive check-one
+
 endif # ENABLE_TESTS
 
 
@@ -2157,7 +2159,7 @@ endif
 # Fake targets.  Always run these rules, even if a file/directory with that
 # name already exists.
 #
-.PHONY: all all_platforms alltags boot checkout chrome realchrome clean clobber clobber_all export install libs makefiles realclean run_viewer run_apprunner tools $(DIRS) $(TOOL_DIRS) FORCE check check-interactive check-one
+.PHONY: all all_platforms alltags boot checkout chrome realchrome clean clobber clobber_all export install libs makefiles realclean run_viewer run_apprunner tools $(DIRS) $(TOOL_DIRS) FORCE
 
 # Used as a dependency to force targets to rebuild
 FORCE:
