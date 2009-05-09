9398a86ee9e00c0f5d2d3388ce16ea3d2ad006fc
Robert Sayre ext:(%2C%20L.%20David%20Baron%20%3Cdbaron%40dbaron.org%3E) <sayrer@gmail.com>
2009-05-08 21:59:25 -0700

    Add the editor to cycle collection.  (Bug 423233)  r+sr=peterv  a=blocking1.9.1+ on bug 420670

--- editor/libeditor/html/nsHTMLEditRules.cpp
+++ editor/libeditor/html/nsHTMLEditRules.cpp
@@ -238,7 +238,7 @@ nsHTMLEditRules::~nsHTMLEditRules()
 
 NS_IMPL_ADDREF_INHERITED(nsHTMLEditRules, nsTextEditRules)
 NS_IMPL_RELEASE_INHERITED(nsHTMLEditRules, nsTextEditRules)
-NS_IMPL_QUERY_INTERFACE3(nsHTMLEditRules, nsIHTMLEditRules, nsIEditRules, nsIEditActionListener)
+NS_IMPL_QUERY_INTERFACE_INHERITED2(nsHTMLEditRules, nsTextEditRules, nsIHTMLEditRules, nsIEditActionListener)
 
 
 /********************************************************
