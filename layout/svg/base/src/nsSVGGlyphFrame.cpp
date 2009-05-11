b1aef548406c9fab945a7d23e3f3632ceaf7c158
Ryo Onodera <ryoqun@gmail.com>
2009-05-11 19:47:01 +0100

    Bug 489718 - image-rendering and text-rendering hints should operate when clipping. r=longsonr a=roc

--- layout/svg/base/src/nsSVGGlyphFrame.cpp
+++ layout/svg/base/src/nsSVGGlyphFrame.cpp
@@ -321,6 +321,15 @@ nsSVGGlyphFrame::PaintSVG(nsSVGRenderState *aContext,
   gfxContext *gfx = aContext->GetGfxContext();
   PRUint16 renderMode = aContext->GetRenderMode();
 
+  switch (GetStyleSVG()->mTextRendering) {
+  case NS_STYLE_TEXT_RENDERING_OPTIMIZESPEED:
+    gfx->SetAntialiasMode(gfxContext::MODE_ALIASED);
+    break;
+  default:
+    gfx->SetAntialiasMode(gfxContext::MODE_COVERAGE);
+    break;
+  }
+
   if (renderMode != nsSVGRenderState::NORMAL) {
 
     gfxMatrix matrix = gfx->CurrentMatrix();
@@ -335,7 +344,6 @@ nsSVGGlyphFrame::PaintSVG(nsSVGRenderState *aContext,
       gfx->SetFillRule(gfxContext::FILL_RULE_WINDING);
 
     if (renderMode == nsSVGRenderState::CLIP_MASK) {
-      gfx->SetAntialiasMode(gfxContext::MODE_ALIASED);
       gfx->SetColor(gfxRGBA(1.0f, 1.0f, 1.0f, 1.0f));
       FillCharacters(&iter, gfx);
     } else {
