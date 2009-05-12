60567a8c826407755db9ee5764b3765b05ba2cfe
Wan-Teh Chang <wtc@google.com>
2009-05-12 13:32:35 -0700

    Bug 492464: Update NSPR from NSPR_4_7_4_RTM to NSPR_4_8_BETA1. blocking1.9.1+ by beltzner

--- nsprpub/pr/tests/reinit.c
+++ nsprpub/pr/tests/reinit.c
@@ -1,4 +1,4 @@
-/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
+/* -*- Mode: C++; tab-width: 8; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
 /* ***** BEGIN LICENSE BLOCK *****
  * Version: MPL 1.1/GPL 2.0/LGPL 2.1
  *
@@ -20,6 +20,7 @@
  * the Initial Developer. All Rights Reserved.
  *
  * Contributor(s):
+ *   Ludovico Cavedon <ludovico.cavedon@gmail.com>
  *
  * Alternatively, the contents of this file may be used under the terms of
  * either the GNU General Public License Version 2 or later (the "GPL"), or
@@ -35,17 +36,33 @@
  *
  * ***** END LICENSE BLOCK ***** */
 
-#ifndef macio_h__
-#define macio_h__
+/* This test verifies that NSPR can be cleaned up and reinitialized. */
 
+#include "nspr.h"
+#include <stdio.h>
 
-PR_BEGIN_EXTERN_C
+int main()
+{
+    PRStatus rv;
 
-OSErr ConvertUnixPathToMacPath(const char *, char **);
-OSErr ConvertUnixPathToFSSpec(const char *unixPath, FSSpec *fileSpec);
+    fprintf(stderr, "Init 1\n");
+    PR_Init(PR_USER_THREAD, PR_PRIORITY_NORMAL, 0);
+    fprintf(stderr, "Cleanup 1\n");
+    rv = PR_Cleanup();
+    if (rv != PR_SUCCESS) {
+        fprintf(stderr, "FAIL\n");
+        return 1;
+    }
 
-PR_END_EXTERN_C
+    fprintf(stderr, "Init 2\n");
+    PR_Init(PR_USER_THREAD, PR_PRIORITY_NORMAL, 0);
+    fprintf(stderr, "Cleanup 2\n");
+    rv = PR_Cleanup();
+    if (rv != PR_SUCCESS) {
+        fprintf(stderr, "FAIL\n");
+        return 1;
+    }
 
-
-#endif /* macio_h__ */
-
+    fprintf(stderr, "PASS\n");
+    return 0;
+}
