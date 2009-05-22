617b3ca2a31da8af9130b3c063f50685afc41b2d
Alexander Surkov <surkov.alexander@gmail.com>
2009-05-22 14:54:19 -0400

    Bug 486899 - Keyboard Accessibility on video element (also audio), r=mconnor
    --HG--
    rename : accessible/tests/mochitest/nsIAccessible_states.js => accessible/tests/mochitest/states.js

--- accessible/tests/mochitest/states.js
+++ accessible/tests/mochitest/states.js
@@ -2,36 +2,38 @@ function testStates(aAccOrElmOrID, aState, aExtraState, aAbsentState)
 {
   var [state, extraState] = getStates(aAccOrElmOrID);
 
+  var id = prettyName(aAccOrElmOrID);
+
   is(state & aState, aState,
-     "wrong state bits for " + aAccOrElmOrID + "!");
+     "wrong state bits for " + id + "!");
 
   if (aExtraState)
     is(extraState & aExtraState, aExtraState,
-       "wrong extra state bits for " + aAccOrElmOrID + "!");
+       "wrong extra state bits for " + id + "!");
 
   if (aAbsentState)
     is(state & aAbsentState, 0,
-       "state bits should not be present in ID " + aAccOrElmOrID + "!");
+       "state bits should not be present in ID " + id + "!");
 
   if (state & STATE_READONLY)
     is(extraState & EXT_STATE_EDITABLE, 0,
-       "Read-only " + aAccOrElmOrID + " cannot be editable!");
+       "Read-only " + id + " cannot be editable!");
 
   if (extraState & EXT_STATE_EDITABLE)
     is(state & STATE_READONLY, 0,
-       "Editable " + aAccOrElmOrID + " cannot be readonly!");
+       "Editable " + id + " cannot be readonly!");
 
   if (state & STATE_COLLAPSED || state & STATE_EXPANDED)
     is(extraState & EXT_STATE_EXPANDABLE, EXT_STATE_EXPANDABLE,
-       "Collapsed or expanded " + aAccOrElmOrID + " should be expandable!");
+       "Collapsed or expanded " + id + " should be expandable!");
 
   if (state & STATE_COLLAPSED)
     is(state & STATE_EXPANDED, 0,
-       "Collapsed " + aAccOrElmOrID + " cannot be expanded!");
+       "Collapsed " + id + " cannot be expanded!");
 
   if (state & STATE_EXPANDED)
     is(state & STATE_COLLAPSED, 0,
-       "Expanded " + aAccOrElmOrID + " cannot be collapsed!");
+       "Expanded " + id + " cannot be collapsed!");
 }
 
 function getStringStates(aAccOrElmOrID)
