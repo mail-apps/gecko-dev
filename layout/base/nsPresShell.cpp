6ee5c4831936ec67571f0748bb94efe52c9bc58c
Boris Zbarsky <bzbarsky@mit.edu>
2009-06-16 11:46:24 -0700

    Bug 498530.  Don't try to remove array elements that might not be in the array.  r+sr=smaug. a=shaver for CLOSED TREE

--- layout/base/nsPresShell.cpp
+++ layout/base/nsPresShell.cpp
@@ -6448,11 +6448,13 @@ PresShell::FireOrClearDelayedEvents(PRBool aFireEvents)
     return;
   }
 
-  if (!mIsDestroying && mDocument) {
+  if (mDocument) {
     nsCOMPtr<nsIDocument> doc = mDocument;
-    while (mDelayedEvents.Length() && !doc->EventHandlingSuppressed()) {
-      mDelayedEvents[0]->Dispatch(this);
+    while (!mIsDestroying && mDelayedEvents.Length() &&
+           !doc->EventHandlingSuppressed()) {
+      nsAutoPtr<nsDelayedEvent> ev(mDelayedEvents[0].forget());
       mDelayedEvents.RemoveElementAt(0);
+      ev->Dispatch(this);
     }
     if (!doc->EventHandlingSuppressed()) {
       mDelayedEvents.Clear();
