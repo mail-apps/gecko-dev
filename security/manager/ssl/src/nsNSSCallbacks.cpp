13f27cfc725afced562829e2059f2b1119c7c4c8
Paul Kehrer <pkehrer@trustwave.com>
2009-07-16 11:40:24 -0400

    Add default OCSP responder for trustwave. b=497184 r=johnath sr=kaie a=beltzner

--- security/manager/ssl/src/nsNSSCallbacks.cpp
+++ security/manager/ssl/src/nsNSSCallbacks.cpp
@@ -1120,6 +1120,12 @@ static struct OCSPDefaultResponders myDefaultOCSPResponders[] = {
     "CN=GlobalSign Extended Validation CA,O=GlobalSign,OU=Extended Validation CA",
     nsnull, "NLH5yYxrNUTMCGkK7uOjuVy/FuA=", nsnull,
     "http://ocsp.globalsign.com/ExtendedSSL"
+  },
+  /* Trustwave */
+  {
+    "CN=SecureTrust CA,O=SecureTrust Corporation,C=US",
+    nsnull, "QjK2FvoE/f5dS3rD/fdMQB1aQ68=", nsnull,
+    "http://ocsp.trustwave.com"
   }
 };
 
