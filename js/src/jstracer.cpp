63feb150f1229a74c00f4cbaea4a528cca0456af
Christian Legnitto <clegnitto@mozilla.com>
2011-06-13 12:53:00 -0700

    Bug 663666 - Backed out changeset d21c370f1009 (backs out bug 650874), a=LegNeato

--- js/src/jstracer.cpp
+++ js/src/jstracer.cpp
@@ -10723,7 +10723,7 @@ TraceRecorder::record_JSOP_CALLGVAR()
 
     jsval& v = STOBJ_GET_SLOT(globalObj, slot);
     stack(0, get(&v));
-    stack(1, INS_CONSTOBJ(globalObj));
+    stack(1, INS_CONSTPTR(NULL));
     return JSRS_CONTINUE;
 }
 
