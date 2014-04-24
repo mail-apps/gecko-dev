fae7a49254ad8dbbb1663bdc04b42b94629d405f
Shu-yu Guo <shu@rfrn.org>
2014-04-23 17:33:44 -0700

    Bug 1000598 - Clear TypeCompartment tables. r=billm, a=sledru
    --HG--
    branch : SEA_2_26_1_RELBRANCH
    extra : transplant_source : V%D4%C53%23%C0%1C%A0%2CV%87%EC%80u%1D%D4i%0A%D2%B6

--- js/src/jscompartment.cpp
+++ js/src/jscompartment.cpp
@@ -665,6 +665,7 @@ JSCompartment::clearTables()
     JS_ASSERT(!gcWeakMapList);
     JS_ASSERT(enumerators->next() == enumerators);
 
+    types.clearTables();
     if (baseShapes.initialized())
         baseShapes.clear();
     if (initialShapes.initialized())
--- js/src/jsinfer.h
+++ js/src/jsinfer.h
@@ -1526,6 +1526,7 @@ struct TypeCompartment
     /* Mark any type set containing obj as having a generic object type. */
     void markSetsUnknown(JSContext *cx, TypeObject *obj);
 
+    void clearTables();
     void sweep(FreeOp *fop);
     void finalizeObjects();
 
